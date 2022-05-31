a7164c0e77e529b2cd7768ecf8b62ab636649442
jli <jli@adrastea.ifi.uni-heidelberg.de>
2022-05-31 20:08:43 +0000

    fix bug.

--- similarity.py
+++ similarity.py
@@ -7,7 +7,7 @@ from typing import NamedTuple
 from datasets import Dataset
 
 
-def compute_similarity(dataset) -> NamedTuple:
+def compute_similarity(dataset, n_gram: int = 2) -> NamedTuple:
     """
     Compute mean and maximum similarities of each sentence in summary compared with each article.
     Input: dataset
@@ -15,12 +15,13 @@ def compute_similarity(dataset) -> NamedTuple:
     """
 
     # use fmeasure to determine similarity
-    aligner = RougeNAligner(n=2, optimization_attribute="fmeasure", lang="en")
+    aligner = RougeNAligner(n=n_gram, optimization_attribute="fmeasure", lang="en")
 
-    similarity = namedtuple("similarity", "mean max")
+    similarity = namedtuple("similarity", "mean m_max max")
 
     mm = []  # mean of mean
     m_max = []  # mean of maximum
+    maxi = 0 # maximum similarity of all summary sentences
 
     for sample in tqdm(dataset):
         m = []  # mean
@@ -28,12 +29,16 @@ def compute_similarity(dataset) -> NamedTuple:
             sample["target"], sample["source"]
         ):
             m.append(aligned_sentence.metric)
+
         if len(m) != 0:  # ignore results of empty rows
             mm.append(sum(m) / len(m))
             m_max.append(max(m))
+            if maxi < max(m):
+                maxi = max(m)
 
     similarity.mean = sum(mm) / len(mm)
-    similarity.max = sum(m_max) / len(m_max)
+    similarity.m_max = sum(m_max) / len(m_max)
+    similarity.max = maxi
 
     return similarity
 
@@ -44,13 +49,14 @@ def load_print(dataset_name: str, version: str, split_: str = "train") -> None:
         dataset = pd.DataFrame(dataset["source"])
         dataset = dataset.rename(columns={"document": "source", "summary": "target"})
         dataset = Dataset.from_pandas(dataset)
-    print(dataset[0])
-    # similarity = compute_similarity(dataset)
 
-    # print(f"[{dataset_name}] Mean similarity of summaries: {similarity.mean:.2f}.")
-    # print(
-    #     f"[{dataset_name}] Mean of maximum similarities of summaries: {similarity.max:.2f}."
-    # )
+    similarity = compute_similarity(dataset)
+
+    print(f"[{dataset_name}] Mean similarity of summaries: {similarity.mean:.2f}.")
+    print(
+        f"[{dataset_name}] Mean of maximum similarities of summaries: {similarity.m_max:.2f}."
+    )
+    print(f"[{dataset_name}] Maximum similarity of all summaries: {similarity.max:.2f}.")
 
 
 # load data and print stats of cnn_dailymail
@@ -60,11 +66,11 @@ def load_print(dataset_name: str, version: str, split_: str = "train") -> None:
 # load_print("xsum", "1.2.0", "train")
 
 # load data and print stats of wiki_lingua English
-load_print("wiki_lingua", "english", "train")
+# load_print("wiki_lingua", "english", "train")
 
 # load data and print stats of scitldr
 # load_print("scitldr", "Abstract", "train")
-# load_print("scitldr", "FullText", "train")
+load_print("scitldr", "FullText", "train")
 
 # load data and print stats of billsum
 # load_print("billsum", "3.0.0", "train")
