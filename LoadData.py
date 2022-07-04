3de5900ddac49e007d57a2cb2a5cd333ecbd0b3a
jli <jli@adrastea.ifi.uni-heidelberg.de>
2022-07-04 19:50:56 +0000

    update LoadData.

--- LoadData.py
+++ LoadData.py
@@ -1,8 +1,10 @@
-from datasets import load_dataset
 import numpy as np
 import argparse
-from typing import List
 import csv
+import json
+from typing import List
+from datasets import load_dataset
+
 
 
 def get_args():
@@ -10,16 +12,16 @@ def get_args():
     parser = argparse.ArgumentParser(
         description="Get different information of different datasets"
     )
-    parser.add_argument("--ds", nargs=2, type=str, help="name of dataset, split")
+    parser.add_argument("-d","--dataset", nargs=2, type=str, help="name of dataset, split")
     parser.add_argument(
-        "--tm", nargs=1, type=str, help="tokenization method: whitespace, spacy"
+        "-t","--tokenmethod", nargs=1, type=str, help="tokenization method: whitespace, spacy"
     )
-    parser.add_argument("--sts", nargs=1, type=str, help="type of stats: simi, len")
+    parser.add_argument("-s","--stats", nargs=1, type=str, help="type of stats: simi, len")
     # parser.add_argument(
     #     "--sf", nargs=1, type=str, help="if save the figure: 1, otherwise: 0"
     # )
     parser.add_argument(
-        "--p", nargs="?", type=float, const=1, help="proportion of samples"
+        "-p","--proporofsa", nargs="?", type=float, const=1, help="proportion of samples"
     )
 
     args = parser.parse_args()
@@ -33,21 +35,19 @@ def rename_datasets(dataset):
     return dataset
 
 
-def load_data(dataset_name: str, split_: str = "train", p: float = 1):
-    version_dic = {
-        "cnn_dailymail": "3.0.0",
-        "xsum": "1.2.0",
-        "wiki_lingua": "english",
-        "scitldr_A": "Abstract",
-        "scitldr_F": "FullText",
-        "billsum": "3.0.0",
-    }
+def load_data(dataset_name: str, split: str = "train", data_proportion: float = 1.0):
+    
+    with open('datasets_dict.txt') as f:
+        dicts = f.read()
+      
+    # reconstructing the data as a dictionary
+    version_dic = json.loads(dicts)
     version = version_dic.get(dataset_name)
 
     if dataset_name == "scitldr_A" or dataset_name == "scitldr_F":
         dataset_name = "scitldr"
 
-    dataset = load_dataset(dataset_name, version, split=split_)
+    dataset = load_dataset(dataset_name, version, split=split)
 
     if dataset_name == "wiki_lingua":
         dataset = dataset.rename_column("article", "source")
@@ -57,8 +57,8 @@ def load_data(dataset_name: str, split_: str = "train", p: float = 1):
         dataset = rename_datasets(dataset)
 
     # use a certain proportion of samples
-    if p != 1:
-        n = np.int64(dataset.num_rows * p)
+    if data_proportion != 1:
+        n = int(dataset.num_rows * data_proportion)
         dataset = dataset.select(np.random.randint(dataset.num_rows, size=n))
 
     return dataset
@@ -79,10 +79,3 @@ def read_csv(filename):
         data[i] = list(map(eval, data[i]))
 
     return data
-
-
-dataset = load_data("wiki_lingua", "train", 0.1)
-dataset.features["source"]
-
-dataset = load_data("wiki_lingua", "train")
-dataset.features["source"]
