dc8a2fa7806d92b66879a0a1800aa0e1004ee319
jli <jli@adrastea.ifi.uni-heidelberg.de>
2022-07-30 20:08:23 +0000

    add test files.

---
+++ tests/test_LoadData.py
@@ -0,0 +1,17 @@
+import unittest
+from LoadData import load_data
+
+
+class TestStats(unittest.TestCase):
+    def test_LoadDate(self):
+        dataset = load_data("wiki_lingua")
+        # the original row of index 14 is empty
+        dataset = dataset.select([0, 14])
+        expected_num_rows = 2
+        expected_features = ["section_name", "source", "target"]
+        self.assertEqual(expected_num_rows, dataset.num_rows)
+        self.assertEqual(expected_features, dataset.column_names)
+
+
+if __name__ == "__main__":
+    unittest.main()
